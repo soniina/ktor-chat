a6981e586f0538e1539f72e796f62464482d3ddc
soniina <sonikpav@gmail.com>
2025-06-21 19:49:40 +0300

    init: basic Ktor setup with WebSocket echo endpoint

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,12 @@
+package ktor.learn
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSockets()
+    configureRouting()
+}
