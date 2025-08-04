be7fac6b8b7545a531ffb3a69d0b698b9e0c9198
soniina <sonikpav@gmail.com>
2025-08-04 22:12:50 +1000

    test: add unit tests for new funcs in UserRepository

--- src/test/kotlin/learn/ktor/repositories/UserRepositoryTest.kt
+++ src/test/kotlin/learn/ktor/repositories/UserRepositoryTest.kt
@@ -16,7 +16,7 @@ class UserRepositoryTest {
     @Test
     fun `should add and get user`() = runTest {
         userRepository.addUser("alice", "password")
-        val user = userRepository.getByUsername("alice")
+        val user = userRepository.getUserByUsername("alice")
         assertNotNull(user)
         assertEquals("alice", user.username)
     }
@@ -31,8 +31,13 @@ class UserRepositoryTest {
 
     @Test
     fun `should return null for non-existent user`() = runTest {
-        val user = userRepository.getByUsername("alice")
-        assertNull(user)
+        val results = listOf(
+            userRepository.getUserByUsername("alice"),
+            userRepository.getUsernameById(99),
+            userRepository.getIdByUsername("alice")
+        )
+
+        results.forEach { assertNull(it) }
     }
 
     @Test
@@ -46,5 +51,23 @@ class UserRepositoryTest {
     fun `should not delete non-existent user`() = runTest {
         val result = userRepository.deleteUser("alice")
         assertFalse(result)
+    }
+
+    @Test
+    fun `should get user id by username`() = runTest {
+        val username = "alice"
+        val user = userRepository.addUser(username, "password")
+
+        assertNotNull(user)
+        assertEquals(user.id, userRepository.getIdByUsername(username))
+    }
+
+    @Test
+    fun `should get username by user id`() = runTest {
+        val username = "alice"
+        val user = userRepository.addUser(username, "password")
+
+        assertNotNull(user)
+        assertEquals(username, userRepository.getUsernameById(user.id))
     }
 }
